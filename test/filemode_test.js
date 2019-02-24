dc0337db9abe0a7b0fa2b1576a6468adcd03098d
okunishinishi <okunishinishi@gmail.com>
2019-02-24 09:27:01 +0900

    remove co

--- test/filemode_test.js
+++ test/filemode_test.js
@@ -7,29 +7,28 @@
 const filemode = require('../lib/filemode.js')
 const fs = require('fs')
 const mkdirp = require('mkdirp')
-const co = require('co')
 
 let tmpDir = __dirname + '/../tmp'
 
 describe('filemode', () => {
-  before(() => co(function * () {
+  before(async () => {
     mkdirp.sync(tmpDir)
-  }))
+  })
 
-  after(() => co(function * () {
-  }))
+  after(async () => {
+  })
 
-  it('Filemode', () => co(function * () {
-    yield filemode(__filename, '644')
-    yield filemode({
+  it('Filemode', async () => {
+    await filemode(__filename, '644')
+    await filemode({
       '*.txt': '644'
     }, { cwd: tmpDir })
-    fs.writeFileSync(tmpDir + '/foo.txt', 'foo');
-    fs.writeFileSync(tmpDir + '/bar.txt', 'bar');
-    yield filemode({
+    fs.writeFileSync(tmpDir + '/foo.txt', 'foo')
+    fs.writeFileSync(tmpDir + '/bar.txt', 'bar')
+    await filemode({
       '*.txt': '444'
     }, { cwd: tmpDir })
-  }))
+  })
 })
 
 /* global describe, before, after, it */
